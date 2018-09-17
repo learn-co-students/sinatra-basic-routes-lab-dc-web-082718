b13ecf547991299f0a21068ec8b8d40b40d28e01
github username <github email address>
2018-09-17 10:33:33 -0400

    Become familiar with get parsing.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Sam-the-driver!"
+  end
+  get '/hometown' do
+    "My hometown is D.C."
+  end
+  get '/favorite-song' do
+    "My favorite song is Jaded by Drake."
+  end
 end
