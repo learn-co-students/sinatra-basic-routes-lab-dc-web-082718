70ab018c0cc02df0e1e7867856dc974f74d4a4c7
Angelica Winkler <carowink3@gmail.com>
2018-09-17 11:08:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Angelica Winkler"
+  end
+
+  get '/hometown' do
+    "My hometown is Bethesda, Maryland"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Moonlight'"
+  end
 end
