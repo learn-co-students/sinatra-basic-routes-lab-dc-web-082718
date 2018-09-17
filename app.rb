08f49a7a1ede4ce5aa32c93c8151880ef77be0a5
Trevor Jameson <github email address>
2018-09-17 10:37:43 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get "/name" do
+    "My name is Trevor"
+  end
+
+  get "/hometown" do
+    "My hometown is Baltimore"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Feather"
+  end
+
 end
